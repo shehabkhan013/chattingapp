9737d2c941f5fd2af292d4c27004f00985f4e0e6
A. M. Shehab Khan <shehabkhan013@gmail.com>
2024-09-03 17:53:09 +0600

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,12 @@
-import Userlist from "../components/Userlist";
 import FriendRequest from "../components/FriendRequest";
 import Friends from "../components/Friends";
+import UserList from "../components/UserList";
 const Home = () => {
   return (
     <>
       <div className="grid grid-cols-1 md:grid-cols-[2fr,4fr] gap-x-0 lg:gap-x-5 xl:gap-x-10">
         <div className="w-full bg-[#FBFBFB]">
-          <Userlist />
+          <UserList />
         </div>
         <div className="w-full grid grid-cols-1 md:grid-cols-2 gap-y-10 md:gap-y-0 gap-x-3 lg:gap-x-5 xl:gap-x-10 md:pr-10">
           <div className="w-full h-full bg-white rounded-md">
